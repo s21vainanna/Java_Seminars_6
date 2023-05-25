ffd477f74058a30bec57e98ed1b73d72ffed835b
localuser <localuser@VEA.LV>
2023-05-25 15:59:54 +0300

    iepušoju

--- src/main/java/lv/venta/controllers/FilterController.java
+++ src/main/java/lv/venta/controllers/FilterController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.services.IFilteringService;
 
@@ -55,4 +56,10 @@ public class FilterController {
 	
 	//TODO filter all courses by student by id
 	//TODO please use ?
+	@GetMapping("/info/showAllCourses/students")
+	public String getshowAllCoursesByStudentId(@RequestParam(name="id") long id, Model model) {
+		try {
+			model.addAttribute("courses", filterService.retrieveService)
+		}
+	}
 }
